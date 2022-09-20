7d2f6842b6c4ca639f5c90f865398850f6bc897d
iceagelemonade <bryanjcarr88@gmail.com>
2022-09-19 20:21:36 -0400

    game board layout and column selector is functional. about to change how gameboard is drawn and want a backup incase I break it

--- script/app.js
+++ script/app.js
@@ -6,22 +6,47 @@
     
 // }
 const gameBoardContainer = document.querySelector('.gameboard-container')
+let isOrientationNormal = true
 
 const startGame = () => {
     document.getElementById('start-button').remove()
     drawGameBoard()
 }
+
+// when a turn starts, before location is selected
+const startTurn = () => {
+    const start = isOrientationNormal?0:1
+    for (i=start; i < 7; i++) {
+        const selector = document.createElement('div')
+        selector.classList.add('selector')
+        selector.setAttribute('id', 'selector'+i)
+        gameBoardContainer.appendChild(selector)
+        isOrientationNormal?undefined:selector.style.gridRow='1 / 9'
+    }
+}
+
 // Below is the function that draws the gameboard after the game begins
 const drawGameBoard = () => {
     // const cover = document.createElement('div')
     // cover.classList.add('tester')
     // gameBoardContainer.appendChild(cover)
     for (i = 0; i < 42; i++) {
+        let prefix = isOrientationNormal?'':'rotated-'
         const newSquare = document.createElement('div')
+        const newSquareOverlay = document.createElement('div')
         newSquare.classList.add('gameboard-space')
-        newSquare.setAttribute('id', 'space'+i)
+        newSquareOverlay.classList.add('overlay')
+        newSquare.setAttribute('id', prefix+'space'+i)
+        newSquareOverlay.setAttribute('id', prefix+'space'+i)
         gameBoardContainer.appendChild(newSquare)
+        gameBoardContainer.appendChild(newSquareOverlay)
     }
+    // remove after testing is done
+    startTurn()
+    // testing shading
+    const shadow = document.createElement('div')
+    shadow.classList.add('shadow')
+    gameBoardContainer.appendChild(shadow)
 }
 
 // Below is the code for the animations in the upper-right and upper-left corners
